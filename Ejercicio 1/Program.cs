1cb838c639d9f9daca4788087d461171a5eac8bd
Diego Henderson <50886236+diegohenderson@users.noreply.github.com>
2019-10-10 23:13:45 -0300

    up.

---
+++ Ejercicio 1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cant;
+            int num;
+            int mayor = 0;
+            int menor = 0;
+            Console.Write("digite la cant de num a comparar");
+            cant = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cant; i++)
+            {
+                Console.WriteLine("escriba un numero");
+                num = int.Parse(Console.ReadLine());
+                if (i==0)
+                {
+                    mayor = num;
+                    menor = num;
+                }
+                else if (i != 0)
+                {
+                    if (num < menor)
+                    {
+                        menor = num;
+                    }
+                    if (num>mayor)
+                    {
+                        mayor = num;
+                    }
+                }
+            }
+            Console.WriteLine("el numero mayor es " + mayor);
+            Console.WriteLine("el numero mayor es " + menor);
+            Console.ReadKey();
+        }
+    }
+}
