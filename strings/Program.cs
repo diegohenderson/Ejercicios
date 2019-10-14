d2e80c8c63ef5668b53c9320228da0c6b6beda89
Diego Henderson <50886236+diegohenderson@users.noreply.github.com>
2019-10-14 12:24:26 -0300

    Sending  a commit.

---
+++ strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena = " asd ->32 ->ads-> gfd-> hola";
+            string[] arreglo = cadena.Split("->".ToCharArray(),StringSplitOptions.RemoveEmptyEntries);//si quiero leer un espacio,dejarlo vacio, sino agregarle simbolo correspondiente
+            //si son 2 simbolos diferentes, usamos tochararray , y podemos remover los espacios
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                Console.WriteLine(arreglo[i]);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tuples/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region array
+            //string[] listacompra = new string[4];
+            //listacompra[0] = "leche";
+            //listacompra[1] = "asd";
+            //listacompra[2] = "13";
+            //listacompra[3] = "le54che";
+            //foreach (string item in listacompra)
+            //{
+            //    Console.Write(item + ",");
+            //}
+            //int[] numeros = { 1, 2, 3, 4, 5, 6 };
+            //int suma = 0;
+            //foreach (int item in numeros)
+            //{
+            //    suma = suma + item;
+            //}
+            //Console.WriteLine(suma);
+            #endregion
+            //List<int> listanumeros = new List<int>();
+            //listanumeros.Add(15);
+            //listanumeros.Add(22);
+            //listanumeros.Add(33);
+            #region muestra y algunos metodos
+            //listanumeros.Insert(0, 99);
+            //listanumeros.Reverse();
+            //if (listanumeros.Contains(15))
+            //{
+            //    Console.WriteLine("si");
+            //}
+            //foreach (int numero in listanumeros)
+            //{
+            //    Console.WriteLine(numero);
+            //}
+            #endregion
+            #region recorrer lista
+            //foreach (int item in listanumeros)
+            //{
+            //    Console.WriteLine(item);
+            //    Console.WriteLine (listanumeros.IndexOf(item));//el indice
+            //}
+            #endregion
+            #region diccionarios
+            //Dictionary<string, string> alumnos = new Dictionary<string, string>();
+            //alumnos.Add("a", "paula");
+            //alumnos.Add("b", "asd");
+            //alumnos.Add("c", "123");
+            ////Console.WriteLine(alumnos["b"]);
+            //alumnos.Remove("a");
+            //foreach (KeyValuePair<string,string >item in alumnos) //el tipo es keyvalue primero key y desp valeu o podes poner var...
+            //{
+            //    Console.WriteLine("clave " + item.Key + ":" + item.Value);
+            //}
+            #endregion
+
+            Console.ReadKey();
+        }
+
+    }
+}
